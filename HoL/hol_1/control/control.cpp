46b3fba1b77b1c77e6bb7a74b38c4521091c020c
somma <fixbrain@gmail.com>
2017-07-25 22:58:31 +0900

    Initial commit

---
+++ HoL/hol_1/control/control.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file    Minifilter driver dev HOL.
+ * @author  somma (fixbrain@gmail.com)
+ * @date    2017/01/25 created.
+ * @copyright All rights reserved by somma.
+**/
+#include "stdafx.h"
+#include "scm_context.h"
+
+/// @brief	Entry point
+int main()
+{	
+	con_msg
+		"\n===============================================================================\n"\
+		"driver Compiled at %s on %s \n"\
+		"===============================================================================\n",
+		__TIME__, __DATE__
+		log_end;
+
+
+    return 0;
+}
+
