2f0da79ea8860020b2965b1d1891bc391530f3cd
somma <fixbrain@gmail.com>
2017-08-22 21:43:26 +0900

    scanner 샘플 추가
    - install, start 등의 기능 추가

---
+++ HoL/scanner/inc/scanuk.h
@@ -0,0 +1,57 @@
+/*++
+
+Copyright (c) 1999-2002  Microsoft Corporation
+
+Module Name:
+
+    scanuk.h
+
+Abstract:
+
+    Header file which contains the structures, type definitions,
+    constants, global variables and function prototypes that are
+    shared between kernel and user mode.
+
+Environment:
+
+    Kernel & user mode
+
+--*/
+
+#ifndef __SCANUK_H__
+#define __SCANUK_H__
+
+//
+//  Name of port used to communicate
+//
+
+const PWSTR ScannerPortName = L"\\ScannerPort";
+
+const PWSTR holFilter = L"scanner.sys";
+const PWSTR holFilterService = L"scanner";
+const PWSTR holFilterServiceDisplay = L"scanner service";
+const PWSTR holFilterAltitude = L"0";
+const int holFilterFlag = 0;
+
+
+
+
+#define SCANNER_READ_BUFFER_SIZE   1024
+
+typedef struct _SCANNER_NOTIFICATION {
+
+    ULONG BytesToScan;
+    ULONG Reserved;             // for quad-word alignement of the Contents structure
+    UCHAR Contents[SCANNER_READ_BUFFER_SIZE];
+    
+} SCANNER_NOTIFICATION, *PSCANNER_NOTIFICATION;
+
+typedef struct _SCANNER_REPLY {
+
+    BOOLEAN SafeToOpen;
+    
+} SCANNER_REPLY, *PSCANNER_REPLY;
+
+#endif //  __SCANUK_H__
+
+
